0268b99e3f2d14b26ca52d19fd0e0b9883ce650d
이홍주 <hongju4385@gmail.com>
2023-05-02 17:39:28 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Firstpage from "./bookmark/bookmark";
+import Firstpage from "./ranking/ranking";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
