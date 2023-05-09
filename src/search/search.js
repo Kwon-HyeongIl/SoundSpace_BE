1c1e6a9c6be71c50bdcca5784d7f27d64b12ae32
gjsk132 <gjsk132@naver.com>
2023-05-09 22:31:31 +0900

    디자인 저장용 커밋

--- src/search/search.js
+++ src/search/search.js
@@ -4,9 +4,12 @@ function UserSearch(){
     return(
         <div className='search_back_color'>
             <div className="search_random">
-                <input className="Searching" type="text" name ="searching" placeholder="Search for User_Name"></input>
-                <button className="find_user"></button>
-                <button className="random_dice"></button>
+                <div className="search_box">
+                    <input className="Searching" type="text" name ="searching" placeholder="Search for User_Name"></input>
+                    <button className="find_user"></button>
+                </div>
+                    <button className="random_dice"></button>
+                
             </div>
             
             <div className="word">Searching result...</div>
@@ -14,7 +17,7 @@ function UserSearch(){
             <div className="search_result">
                 <div className="user_UI"></div>
                 <div className="user_ID">PKNU_WAP</div>
-                <div className="like">1004</div>
+                <div id="like">1004</div>
             </div>
         </div>
     );
