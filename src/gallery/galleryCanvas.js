945913761e3906e8ab4b566a7d74e66c9c29de54
이홍주 <hongju4385@gmail.com>
2023-05-13 19:14:01 +0900

    벽 만드는 중 -#2

--- src/gallery/galleryCanvas.js
+++ src/gallery/galleryCanvas.js
@@ -1,82 +1,3 @@
-//learning-threejs 예제 불러오는 코드
-
-//
-// function GalleryCanvas() {
-//   const canvasRef = useRef(null);
-
-//   useEffect(() => {
-//     const canvas = canvasRef.current;
-//     const renderer = new THREE.WebGLRenderer({ canvas });
-//     renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-
-//     const scene = new THREE.Scene();
-
-//     const camera = new THREE.PerspectiveCamera(
-//       45,
-//       canvas.clientWidth / canvas.clientHeight,
-//       0.1,
-//       1000
-//     );
-//     camera.position.set(-30, 40, 30);
-//     camera.lookAt(scene.position);
-
-//     const planeGeometry = new THREE.PlaneGeometry(60, 20);
-//     const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-//     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-//     plane.receiveShadow = true;
-//     plane.rotation.x = -0.5 * Math.PI;
-//     plane.position.x = 15;
-//     plane.position.y = 0;
-//     plane.position.z = 0;
-//     scene.add(plane);
-
-//     const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-//     const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-//     const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-//     cube.castShadow = true;
-//     cube.position.x = -4;
-//     cube.position.y = 3;
-//     cube.position.z = 0;
-//     scene.add(cube);
-
-//     const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-//     const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-//     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-//     sphere.castShadow = true;
-//     sphere.position.x = 20;
-//     sphere.position.y = 4;
-//     sphere.position.z = 2;
-//     scene.add(sphere);
-
-//     const spotLight = new THREE.SpotLight(0xffffff);
-//     spotLight.position.set(-40, 60, -10);
-//     spotLight.castShadow = true;
-//     scene.add(spotLight);
-
-//     function render() {
-//       renderer.render(scene, camera);
-//     }
-
-//     function animate() {
-//       requestAnimationFrame(animate);
-//       cube.rotation.x += 0.01;
-//       cube.rotation.y += 0.01;
-//       sphere.rotation.x += 0.01;
-//       sphere.rotation.y += 0.01;
-//       render();
-//     }
-
-//     animate();
-//   }, []);
-
-//   return <canvas id="gallery-canvas" ref={canvasRef} />;
-// }
-
-// export default GalleryCanvas;
-
-//https://www.youtube.com/watch?v=9ZEjSxDRIik&t=5s 예제
-
-// import { createRoot } from "react-dom/client";
 import React, { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import "./galleryCanvas.css";
@@ -92,8 +13,8 @@ import Player from "./Player.js";
 import { Text } from "@react-three/drei";
 import { FPV } from "./FPV";
 
-function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+function Box({ position }) {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 0, 0] }));
 
   useFrame(() => {
     if (api && api.velocity) {
@@ -110,7 +31,7 @@ function Box() {
 
   return (
     <group>
-      <mesh ref={ref} position={[0, 2, 0]}>
+      <mesh ref={ref} position={[0, 2, 0]} castShadow>
         <boxBufferGeometry attach="geometry" />
         <meshLambertMaterial attach="material" color="hotpink" />
       </mesh>
@@ -153,6 +74,7 @@ const Button = () => {
       onPointerOver={handlePointerOver}
       onPointerOut={handlePointerOut}
       position={[-1, 0, 0]}
+      castShadow
     >
       <boxBufferGeometry args={[0.2, 0.2, 0.2]} />
       <meshStandardMaterial map={texture} color={isHovered ? "red" : "blue"} />
@@ -167,9 +89,37 @@ function PlaneBottom() {
   }));
   return (
     // <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-    <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+    <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2} receiveShadow>
       <planeBufferGeometry attach="geometry" args={[10000, 10000]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
+      <meshLambertMaterial attach="material" color="gray" />
+    </mesh>
+  );
+}
+
+function PlaneRight() {
+  const [ref] = usePlane(() => ({
+    rotation: [0, Math.PI / 2, 0], // Rotate the plane 90 degrees around the y-axis
+    position: [-10, 0, 0], // Adjust the position to move it to the right side
+  }));
+
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[10, 10]} />
+      <meshLambertMaterial attach="material" color="yellow" />
+    </mesh>
+  );
+}
+
+function PlaneLeft() {
+  const [ref] = usePlane(() => ({
+    rotation: [0, 0, Math.PI / 2], // Rotate the plane 90 degrees around the y-axis
+    position: [0, 0, -10], // Adjust the position to move it to the right side
+  }));
+
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[10, 10]} />
+      <meshLambertMaterial attach="material" color="purple" />
     </mesh>
   );
 }
@@ -208,7 +158,7 @@ function PlaneBottom() {
 export default function GalleryCanvas() {
   return (
     <>
-      <Canvas shadows camera={{ fov: 45 }}>
+      <Canvas shadows={true} camera={{ fov: 45 }}>
         {/* <OrbitControls /> */}
         <FirstPersonControls
           lookSpeed={0.07}
@@ -221,12 +171,15 @@ export default function GalleryCanvas() {
         <ambientLight intensity={0.5} />
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
         <Stars />
-        <spotLight position={[0, 15, 10]} angle={0.3} />
+        <spotLight position={[0, 15, 10]} angle={0.3} castShadow />
         <FPV />
         <Physics gravity={[0, -30, 0]}>
           <Player />
           <PlaneBottom />
           <Box />
+          {/* <Box position={[-40, 0, -40]} /> */}
+          <PlaneRight />
+          <PlaneLeft />
         </Physics>
       </Canvas>
       <div className="absolute centered cursor">+</div>
