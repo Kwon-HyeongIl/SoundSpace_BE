1aae8c849d745d38539598ec2888caf2196bc990
gjsk132 <gjsk132@naver.com>
2023-05-13 19:20:37 +0900

    유저검색 디자인 및 기능 구현

--- src/search/search.js
+++ src/search/search.js
@@ -1,49 +1,60 @@
 import "./search.css"
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function UserSearch(){
+function UserSearch() {
 
-    const [showResult, setShowResult] = useState(false);
-    const [userInput, setUserInput] = useState("");
-    const [list, setList] = useState(["PKNU_WAP","Hongju","WAP"]);
+  const [showResult, setShowResult] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const idList = [
+    { id: "PKNU_WAP", like: 1004 },
+    { id: "Hongju", like: 9413 },
+    { id: "WAP", like: 2365 }
+  ];
 
-    function findButtonClick() {
-        setShowResult(list.includes(userInput));
+  const [searchResult, setSearchResult] = useState(null);
+
+  function findButtonClick() {
+    const index = idList.findIndex((item) => item.id === userInput);
+    if (index !== -1) {
+      setSearchResult(idList[index].like);
+    } else {
+      setSearchResult(null);
     }
+    setShowResult(true);
+  }
 
-    function findInputChange(event) {
-        setUserInput(event.target.value);   
-    }
-    
-    return(
-        <div className='search_back_color'>
+  function findInputChange(event) {
+    setUserInput(event.target.value);
+    setSearchResult(null); // 검색어가 변경될 때마다 결과 초기화
+  }
 
-            <div className="search_random">
-                <div className="search_box">
-                    <input className="Searching" type="text" name ="searching" placeholder="Search for User_Name" value={userInput} onChange={findInputChange}></input>
-                    <button className="find_user" onClick={findButtonClick}></button>
-                </div>
-                    <button className="random_dice"></button>
-            </div>
+  return (
+    <div className='search_back_color'>
 
-            {showResult &&
-                <div className="word">Searching Result !</div>
-            }
-            {showResult &&
-                <div className="search_result">
-                    <div className="user_UI"></div>
-                    <div className="user_ID">PKNU_WAP</div>
-                    <div id="like">1004</div>
-                </div>
-            };
+      <div className="search_random">
+        <div className="search_box">
+          <input className="Searching" type="text" name="searching" placeholder="Search for User_Name" value={userInput} onChange={findInputChange}></input>
+          <button className="find_user" onClick={findButtonClick}></button>
         </div>
-    );
+        <button className="random_dice"></button>
+      </div>
+
+      {searchResult !== null &&
+        <div className="search_result">
+          <div className="user_UI"></div>
+          <div className="user_ID">{userInput}</div>
+          <div className="heart"></div>
+          <div className="user_like">{searchResult}</div>
+        </div>
+      }
+    </div>
+  );
 }
 
-export default function Searching(){
-    return (
-      <div>
-        <UserSearch></UserSearch>
-      </div>
-    )
-  }
+export default function Searching() {
+  return (
+    <div>
+      <UserSearch></UserSearch>
+    </div>
+  )
+}
