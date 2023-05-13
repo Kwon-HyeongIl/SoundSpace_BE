6efc00ed6b567256c12e643e92b11d687156860e
이홍주 <hongju4385@gmail.com>
2023-05-13 17:48:31 +0900

    물리 충돌 구현, player 질량 추가

--- src/gallery/Player.js
+++ src/gallery/Player.js
@@ -1,63 +1,70 @@
-import * as THREE from "three";
-import * as RAPIER from "@dimforge/rapier3d-compat";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-import { useKeyboardControls } from "@react-three/drei";
-import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
-// import Axe from "./Axe"
+import { useEffect, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useSphere } from "@react-three/cannon";
+import { Vector3 } from "three";
+import useKeyboard from "./useKeyboard"; // Import the useKeyboard hook
 
-const SPEED = 5;
-const direction = new THREE.Vector3();
-const frontVector = new THREE.Vector3();
-const sideVector = new THREE.Vector3();
-const rotation = new THREE.Vector3();
+const JUMP_FORCE = 4;
+const SPEED = 4; // + 이동 SPEED를 위해 추가
+const Player = () => {
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboard(); // + 기존에 actions로 통칭되던 요소를 개별요소를 직접 다 가져오도록 변경
+  const { camera } = useThree();
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: "Dynamic",
+    position: [0, 1, 10],
+  }));
 
-export default function Player({ lerp = THREE.MathUtils.lerp }) {
-  //   const axe = useRef()
-  const ref = useRef();
-  const rapier = useRapier();
-  const [, get] = useKeyboardControls();
-  useFrame((state) => {
-    const { forward, backward, left, right, jump } = get();
-    const velocity = ref.current.linvel();
-    // update camera
-    state.camera.position.set(...ref.current.translation());
-    // update axe
-    // axe.current.children[0].rotation.x = lerp(axe.current.children[0].rotation.x, Math.sin((velocity.length() > 1) * state.clock.elapsedTime * 10) / 6, 0.1)
-    // axe.current.rotation.copy(state.camera.rotation)
-    // axe.current.position.copy(state.camera.position).add(state.camera.getWorldDirection(rotation).multiplyScalar(1))
-    // movement
-    frontVector.set(0, 0, backward - forward);
-    sideVector.set(left - right, 0, 0);
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((v) => (vel.current = v));
+  }, [api.velocity]);
+
+  const pos = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.position.subscribe((p) => (pos.current = p));
+  }, [api.position]);
+
+  useFrame(() => {
+    camera.position.copy(
+      new Vector3(pos.current[0], pos.current[1], pos.current[2])
+    );
+    // + 여기서부터 새로 추가될 코드들
+
+    const direction = new Vector3();
+
+    // z(정면)축 기준으로 이동 계산
+    // 만약 앞+뒤가 동시에 눌리면 -를 통해 값을 0으로 만들어 이동하지 않음
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    // x축 기준 이동 계산
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
     direction
       .subVectors(frontVector, sideVector)
       .normalize()
       .multiplyScalar(SPEED)
-      .applyEuler(state.camera.rotation);
-    ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
-    // jumping
-    const world = rapier.world.raw();
-    const ray = world.castRay(
-      new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 })
-    );
-    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
-    if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 });
+      .applyEuler(camera.rotation);
+
+    // 이동 속도를 반영
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+    // 여기까지 추가!
+
+    // 아래 if 조건에서 기존에 actions.jump를 그냥 jump로 대체
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
   });
-  return (
-    <>
-      <RigidBody
-        ref={ref}
-        colliders={false}
-        mass={1}
-        type="dynamic"
-        position={[0, 10, 0]}
-        enabledRotations={[false, false, false]}
-      >
-        <CapsuleCollider args={[0.75, 0.5]} />
-      </RigidBody>
-      {/* <group ref={axe} onPointerMissed={(e) => (axe.current.children[0].rotation.x = -0.5)}> */}
-      {/* <Axe position={[0.3, -0.35, 0.5]} /> */}
-      {/* </group> */}
-    </>
-  );
-}
+  return <mesh ref={ref}></mesh>;
+};
+
+export default Player;
--- src/gallery/galleryCanvas.js
+++ src/gallery/galleryCanvas.js
@@ -77,10 +77,10 @@
 //https://www.youtube.com/watch?v=9ZEjSxDRIik&t=5s 예제
 
 // import { createRoot } from "react-dom/client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import "./galleryCanvas.css";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import {
   FirstPersonControls,
   Stars,
@@ -88,19 +88,17 @@ import {
   KeyboardControls,
 } from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
-import Ground from "./Ground.js";
 import Player from "./Player.js";
 import { Text } from "@react-three/drei";
-// import "./styles.css";
+import { FPV } from "./FPV";
 
 function Box() {
   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
 
   useFrame(() => {
-    // 사용자의 이동 방향과 속도를 감지하여 충돌 처리
     if (api && api.velocity) {
       const { forward, backward, left, right } = api.velocity;
-      const speed = 0.2; // 이동 속도 조절
+      const speed = 0.2;
       if (forward && forward[2] > 0)
         api.velocity.set(forward[0], forward[1], 0);
       if (backward && backward[2] < 0)
@@ -124,9 +122,43 @@ function Box() {
       >
         go forward!
       </Text>
+      <Button />
     </group>
   );
 }
+
+const Button = () => {
+  const texture = useLoader(
+    THREE.TextureLoader,
+    "https://cdn-icons-png.flaticon.com/512/149/149125.png"
+  );
+  const buttonRef = useRef();
+  const [isHovered, setHovered] = useState(false);
+
+  const handlePointerOver = () => {
+    setHovered(true);
+  };
+
+  const handlePointerOut = () => {
+    setHovered(false);
+  };
+
+  // useFrame(() => {
+  //   buttonRef.current.rotation.y += 0.01;
+  // });
+
+  return (
+    <mesh
+      ref={buttonRef}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
+      position={[-1, 0, 0]}
+    >
+      <boxBufferGeometry args={[0.2, 0.2, 0.2]} />
+      <meshStandardMaterial map={texture} color={isHovered ? "red" : "blue"} />
+    </mesh>
+  );
+};
 
 function PlaneBottom() {
   const [ref] = usePlane(() => ({
@@ -137,17 +169,6 @@ function PlaneBottom() {
     // <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
     <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
       <planeBufferGeometry attach="geometry" args={[10000, 10000]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
-function PlaneRight() {
-  // const [ref] = usePlane(() => ({
-  //   rotation: [-Math.PI / 2, 0, 0],
-  // }));
-  return (
-    <mesh position={[50, 10, 0]} rotation={[-Math.PI / 2, 90, 0]}>
-      <planeBufferGeometry attach="geometry" args={[50, 100]} />
       <meshLambertMaterial attach="material" color="lightblue" />
     </mesh>
   );
@@ -186,15 +207,7 @@ function PlaneRight() {
 
 export default function GalleryCanvas() {
   return (
-    <KeyboardControls
-      map={[
-        { name: "forward", keys: ["ArrowUp", "w", "W"] },
-        { name: "backward", keys: ["ArrowDown", "s", "S"] },
-        { name: "left", keys: ["ArrowLeft", "a", "A"] },
-        { name: "right", keys: ["ArrowRight", "d", "D"] },
-        { name: "jump", keys: ["Space"] },
-      ]}
-    >
+    <>
       <Canvas shadows camera={{ fov: 45 }}>
         {/* <OrbitControls /> */}
         <FirstPersonControls
@@ -209,13 +222,14 @@ export default function GalleryCanvas() {
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
         <Stars />
         <spotLight position={[0, 15, 10]} angle={0.3} />
+        <FPV />
         <Physics gravity={[0, -30, 0]}>
-          {/* <Player /> */}
+          <Player />
           <PlaneBottom />
           <Box />
-          {/* <Ground /> */}
         </Physics>
       </Canvas>
-    </KeyboardControls>
+      <div className="absolute centered cursor">+</div>
+    </>
   );
 }
