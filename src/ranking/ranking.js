d0ebe3b371c03671aef55500fd86ea6d29ae86c4
이홍주 <hongju4385@gmail.com>
2023-05-25 19:36:16 +0900

    Route 연결

--- src/ranking/ranking.js
+++ src/ranking/ranking.js
@@ -2,68 +2,7 @@ import { React, useState, useEffect, useRef } from "react";
 import "./ranking.css";
 import Sidebar from "../sidebar/newSidebar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-//상단네비게이션바
-function NavBar() {
-  const [workOpen, setWork] = useState(false);
-  const [likeOpen, setLike] = useState(false);
-  const workToggle = () => {
-    setWork((workOpen) => !workOpen);
-  };
-  const likeToggle = () => {
-    setLike((likeOpen) => !likeOpen);
-  };
-
-  return (
-    <nav>
-      <div className="navbar">
-        <Sidebar>
-          <div className="userInfo">
-            <div className="userImg"></div>
-            <div className="userName">User Name</div>
-          </div>
-          <div className="sideContent">
-            <div className="itemContainer">CHANGE INFO</div>
-            <div className="itemContainer" onClick={() => workToggle()}>
-              WORK MANAGER
-              {workOpen ? (
-                <ul className="item">
-                  <li href="#">Add Work</li>
-                  <li href="#">Edit Gallery</li>
-                </ul>
-              ) : (
-                <span></span>
-              )}
-            </div>
-            <div className="itemContainer" onClick={() => likeToggle()}>
-              LIKE
-              {likeOpen ? (
-                <ul className="item">
-                  <li>For me</li>
-                  <li>For others</li>
-                </ul>
-              ) : (
-                <span></span>
-              )}
-            </div>
-            <button className="itemContainer">BOOK MARK</button>
-            <button className="itemContainer">RANKING</button>
-          </div>
-        </Sidebar>
-        <div className="logo">
-          <sapn className="logo_f">S</sapn>OUND{" "}
-          <span className="logo_f">S</span>PACE
-        </div>
-        <a className="navitem" href="#">
-          SEARCH
-        </a>
-        <a className="navitem" href="#">
-          RANKING
-        </a>
-      </div>
-    </nav>
-  );
-}
+import NavBar from "../gallery/topNaviBar.js";
 
 function Ranking() {
   return (
