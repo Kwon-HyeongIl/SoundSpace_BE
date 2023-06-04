496441063188fa54e87a3103a5d87aa2ce9e1ae1
이홍주 <hongju4385@gmail.com>
2023-06-04 21:53:41 +0900

    test

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,24 @@
 import axios from "axios";
+import { refresh, refreshErrorHandle } from "../context/refresh";
 
 export default axios.create({
   baseURL: "http://localhost:3000",
+  params: {},
 });
+
+// const Api =  axios.create({
+//   baseURL: "http://localhost:3000",
+//   params: {},
+// });
+
+axios.interceptors.request.use(refresh, refreshErrorHandle);
+
+// export default Api
+
+// 유저정보 api 호출
+// import axios from "../api/axios";
+// const getInfo = async (username:String, passward:string) => {
+//   const {data} = await axios.get("/auth/getInfo");
+
+//   return data;
+// }
