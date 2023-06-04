e924150ef0dbc13ede6c366f9248c7f0f02b2278
gjsk132 <gjsk132@naver.com>
2023-06-04 17:51:18 +0900

    edit 부분 수정

--- src/work/edit.js
+++ src/work/edit.js
@@ -1,35 +1,48 @@
-import { React} from "react";
+import React from "react";
 import "./edit.css";
 import NavBar from "../gallery/topNaviBar.js";
 
 function Ranking() {
+  const my_playlist = [
+    { title: "Love dive_1", artist: "1" },
+    { title: "Ditto", artist: "2" },
+    { title: "hype boy_1", artist: "3" },
+    { title: "Love dive_2", artist: "4" },
+    { title: "Ditto_2", artist: "5" },
+    { title: "hype boy_2", artist: "6" },
+    { title: "Love dive", artist: "7" },
+    { title: "Ditto_1", artist: "8" },
+    { title: "hype boy", artist: "9" },
+    { title: "", artist: ""}
+  ];
+
   return (
     <div className="Background_edit">
       <div className="Frame">
         <div className="Grid">
-
           <div className="edit_head">
-            <div className="edit_head"id="numbering_head">NO</div>
+            <div className="edit_head" id="numbering_head">
+              NO
+            </div>
             <div className="edit_head">MUSIC</div>
             <div className="edit_head">ARTIST</div>
             <div className="edit_head">REMOVE</div>
           </div>
 
-          {Array.from({ length: 10 }, (_, i) => (
-            <div>
+          {my_playlist.map((item, index) => (
+            <div key={index}>
               <hr className="edit-hr" />
-              <div className="edit_content" key={i}>
-                <span className="editItem" id="numbering">{i + 1}</span>
-                <span className="editItem">Ditto</span>
-                <span className="editItem">NewJeans</span>
-                <botton className="remove_mucis"> - </botton>
+              <div className="edit_content">
+                <span className="editItem" id="numbering">
+                  {index + 1}
+                </span>
+                <span className="editItem">{item.title}</span>
+                <span className="editItem">{item.artist}</span>
+                <button className="remove_music"> - </button>
               </div>
             </div>
           ))}
-          <div className="add_music">
-            EDIT PLAYLIST
-          </div>
-
+          <div className="add_music">EDIT PLAYLIST</div>
         </div>
       </div>
     </div>
