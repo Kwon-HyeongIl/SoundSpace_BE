00317c3b30dedd55aa2a9d771f221b53cbf9ad50
KHI <llioopv@naver.com>
2023-06-05 11:23:04 +0900

    코드 오류 수정

--- sound-space/src/main/java/com/example/soundspace/api/v1/controller/WebController.java
+++ sound-space/src/main/java/com/example/soundspace/api/v1/controller/WebController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping(value =  {"", "/notice","/list", "/introduce", "/smallbus", "/limousine", "/bigbus", "/request", "/search", "/search/my"})
+    @GetMapping(value = {"", "/notice", "/list", "/introduce", "/smallbus", "/limousine", "/bigbus", "/request", "/search", "/search/my"})
     public String forward() {
         return "forward:/index.html";
+    }
+}
